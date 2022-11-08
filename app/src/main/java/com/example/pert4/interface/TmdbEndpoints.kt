81e04c4342df924119b96ba448819aaefb00e793
Wildan <wdesnandar@gmail.com>
2022-11-08 19:10:41 +0700

    Tugas List Movie

---
+++ app/src/main/java/com/example/pert4/interface/TmdbEndpoints.kt
@@ -0,0 +1,12 @@
+package com.example.pert4.`interface`
+
+import com.example.pert4.models.PopularMovies
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TmdbEndpoints {
+
+    @GET("/3/movie/popular")
+    fun getMovies(@Query("api_key") key: String): Call<PopularMovies>
+}
